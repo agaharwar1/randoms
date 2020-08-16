2aca629456fda82b72f338fdd54efeec8e1254f2
Animesh Gaharwar <agaharwar1@gmail.com>
2020-08-17 02:54:45 +0530

    Completed

--- Threading/threading.cpp
+++ Threading/threading.cpp
@@ -2,9 +2,6 @@
 #include<thread>
 using namespace std;
 
-
-
-
 void hundred()
 {
 	for(int i = 0 ; i<=1000000;++i)
@@ -13,6 +10,7 @@ void hundred()
 	}
 }
 
+
 void twohundred()
 {
 	for(int j = 1000001;j<=2000000;++j)
@@ -20,15 +18,12 @@ void twohundred()
 		cout<<j<<"\n";
 	}
 }
+
+
 int main()
 {
-	
-
 	thread t1(hundred);
 	thread t2(twohundred);
 	t1.join();
 	t2.join();
-	
-
-
 }
